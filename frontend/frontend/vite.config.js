4618976c66c08d52d2d320f5ca64773433e29f2c
jnkarthikeya <karthikeyajagarlapudi@gmail.com>
2026-04-17 22:28:39 +0530

    Configure server and preview host settings

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true
+  },
   preview: {
     host: true,
-    allowedHosts: "all"
+    allowedHosts: true
   }
 })
